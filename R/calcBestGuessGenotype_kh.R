5f7f4d95bc62b4c84cc143ad473f70d9ba12cc81
KatrinHorn <katrin.horn@imise.uni-leipzig.de>
2022-11-10 13:32:16 +0100

    This my homework, a little test script for Git and GitHub.

---
+++ R/calcBestGuessGenotype_kh.R
@@ -0,0 +1,9 @@
+#' @title Best Guess Genotype
+#' @description This is a little test script
+#' @param data a matrix with genotype data in impute2 format (triples with AA, AB or BB genotype chances)
+#' @param threshold a cut off parameter for determining the best guess genotype according to genotype values for AA, AB and BB
+
+calcBestGuessGenotype_kh = function(data, threshold) {
+  ##
+  print("This function still needs to be implemented.")
+}
