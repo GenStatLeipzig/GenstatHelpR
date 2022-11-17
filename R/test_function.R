373727a8a3166dc8122354a52e9c9943652cc707
max <max.riedl@imise.uni-leipzig.de>
2022-11-17 13:12:35 +0100

    added Oxygen doc

--- R/test_function.R
+++ R/test_function.R
@@ -1,4 +1,18 @@
-#Script to print "test"
+#' @title test function
+#' @description function to print "test"
+#' @param x this parameter does nothing
+#' @return prints "test"
+#' @details DETAILS
+#' @examples 
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname test_function
+#' @export 
+#' 
 test_function <- function(x){
     print("test")
 }
+
